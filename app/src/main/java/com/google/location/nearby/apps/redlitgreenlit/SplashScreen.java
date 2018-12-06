b05630faf51c603cd97d2dc750c8a98ffefeb36a
Nadya <nadyaf.bachtiar@gmail.com>
2018-12-07 00:15:40 +0700

    ui

--- app/src/main/java/com/google/location/nearby/apps/redlitgreenlit/SplashScreen.java
+++ app/src/main/java/com/google/location/nearby/apps/redlitgreenlit/SplashScreen.java
@@ -2,6 +2,8 @@ package com.google.location.nearby.apps.redlitgreenlit;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,6 +19,29 @@ public class SplashScreen extends AppCompatActivity {
         setContentView(R.layout.activity_splash);
         b = findViewById(R.id.start_button);
         et = findViewById(R.id.name);
+
+        et.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(s.toString().trim().length() != 0) {
+                    b.setTextColor(getResources().getColor(R.color.colorPrimaryDark, null));
+                    b.setEnabled(true);
+                } else {
+                    b.setTextColor(getResources().getColor(R.color.colorSecondaryDark, null));
+                    b.setEnabled(false);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
 
         b.setOnClickListener(new View.OnClickListener() {
             @Override
